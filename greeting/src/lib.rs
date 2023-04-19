ebc633e8b04c10dfe72abf60180114885214c254
Antonio Cavestro <antonio.cavestro@prima.it>
2023-04-19 17:28:36 +0200

    Add requirement 2

--- greeting/src/lib.rs
+++ greeting/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn greet(name: String) -> String {
-    format!("Hello, {}.", name)
+pub fn greet(name: Option<String>) -> String {
+    format!("Hello, {}.", name.unwrap_or(String::from("my friend")))
 }
 
 #[cfg(test)]
@@ -8,7 +8,13 @@ mod tests {
 
     #[test]
     fn requirement_1() {
-        let result = greet(String::from("Bob"));
+        let result = greet(Some(String::from("Bob")));
         assert_eq!(result, String::from("Hello, Bob."));
+    }
+
+    #[test]
+    fn requirement_2() {
+        let result = greet(None);
+        assert_eq!(result, String::from("Hello, my friend."));
     }
 }

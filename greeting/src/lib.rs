0a9aa17cbafe33fd9f3c0a587c452e7dde8ed188
Antonio Cavestro <antonio.cavestro@prima.it>
2023-04-21 12:15:04 +0200

    Requirement 4

--- greeting/src/lib.rs
+++ greeting/src/lib.rs
@@ -1,12 +1,19 @@
-pub fn greet(name: Option<String>) -> String {
-    name.map(|n| {
-        if n == n.to_uppercase() {
-            format!("HELLO {}!", n)
-        } else {
-            format!("Hello, {}.", n)
+pub fn greet(name: Vec<String>) -> String {
+    let first = name.first();
+    let second = if name.len() == 2 { name.last() } else { None };
+    match (first, second) {
+        (Some(first_name), None) => {
+            if *first_name == first_name.to_uppercase() {
+                format!("HELLO {}!", first_name)
+            } else {
+                format!("Hello, {}.", first_name)
+            }
         }
-    })
-    .unwrap_or(String::from("Hello, my friend."))
+        (Some(first_name), Some(second_name)) => {
+            format!("Hello, {} and {}.", first_name, second_name)
+        }
+        _ => String::from("Hello, my friend."),
+    }
 }
 
 #[cfg(test)]
@@ -15,19 +22,25 @@ mod tests {
 
     #[test]
     fn requirement_1() {
-        let result = greet(Some(String::from("Bob")));
+        let result = greet(vec![String::from("Bob")]);
         assert_eq!(result, String::from("Hello, Bob."));
     }
 
     #[test]
     fn requirement_2() {
-        let result = greet(None);
+        let result = greet(vec![]);
         assert_eq!(result, String::from("Hello, my friend."));
     }
 
     #[test]
     fn requirement_3() {
-        let result = greet(Some(String::from("JERRY")));
+        let result = greet(vec![String::from("JERRY")]);
         assert_eq!(result, String::from("HELLO JERRY!"));
+    }
+
+    #[test]
+    fn requirement_4() {
+        let result = greet(vec![String::from("Jill"), String::from("Jane")]);
+        assert_eq!(result, String::from("Hello, Jill and Jane."));
     }
 }

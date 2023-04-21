61e08ea7ef2823976b6c78b3731a97a56d976a74
Antonio Cavestro <antonio.cavestro@prima.it>
2023-04-21 11:55:57 +0200

    Requirement 3

--- greeting/src/lib.rs
+++ greeting/src/lib.rs
@@ -1,5 +1,12 @@
 pub fn greet(name: Option<String>) -> String {
-    format!("Hello, {}.", name.unwrap_or(String::from("my friend")))
+    name.map(|n| {
+        if n == n.to_uppercase() {
+            format!("HELLO {}!", n)
+        } else {
+            format!("Hello, {}.", n)
+        }
+    })
+    .unwrap_or(String::from("Hello, my friend."))
 }
 
 #[cfg(test)]
@@ -16,5 +23,11 @@ mod tests {
     fn requirement_2() {
         let result = greet(None);
         assert_eq!(result, String::from("Hello, my friend."));
+    }
+
+    #[test]
+    fn requirement_3() {
+        let result = greet(Some(String::from("JERRY")));
+        assert_eq!(result, String::from("HELLO JERRY!"));
     }
 }
